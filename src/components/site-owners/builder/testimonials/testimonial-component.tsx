8d7da748e2fa6b6d4ebca532c0acd6e37496dcb2
bibek1414 <bbhattarai770@gmail.com>
2025-11-14 13:20:16 +0545

    fix(testomonial-7): fixed design

--- src/components/site-owners/builder/testimonials/testimonial-component.tsx
+++ src/components/site-owners/builder/testimonials/testimonial-component.tsx
@@ -511,27 +511,7 @@ export const TestimonialsComponent: React.FC<TestimonialsComponentProps> = ({
   if (style === "card-7") {
     return (
       <>
-        <style>{`
-          @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap');
-        `}</style>
         <section className="mx-auto flex max-w-6xl flex-col items-start px-6 py-12 text-sm md:px-16 md:py-16 lg:px-24">
-          <div className="mr-auto flex items-center gap-2 rounded-full bg-indigo-50 px-3 py-1 text-indigo-600">
-            <svg
-              width="13"
-              height="14"
-              viewBox="0 0 13 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1.613 8.2a.62.62 0 0 1-.553-.341.59.59 0 0 1 .076-.637l6.048-6.118a.31.31 0 0 1 .375-.069c.061.033.11.084.137.147a.3.3 0 0 1 .014.197L6.537 4.991a.59.59 0 0 0 .07.552.61.61 0 0 0 .504.257h4.276a.62.62 0 0 1 .553.341.59.59 0 0 1-.076.637l-6.048 6.119a.31.31 0 0 1-.375.067.295.295 0 0 1-.15-.344l1.172-3.61a.59.59 0 0 0-.07-.553.61.61 0 0 0-.504-.257z"
-                stroke="#1E4BAF"
-                strokeMiterlimit="5.759"
-                strokeLinecap="round"
-              />
-            </svg>
-            <span>Testimonials</span>
-          </div>
           <h1
             className="mt-4 bg-gradient-to-r from-slate-800 to-slate-500 bg-clip-text text-3xl font-medium text-transparent"
             dangerouslySetInnerHTML={{ __html: title }}
